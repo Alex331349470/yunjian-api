7b647af2f358305da0722a45979b07c3425785f1
alex chan <331349470@qq.com>
2021-01-08 03:40:57 +0000

    add background to yunjian-api

--- app/Admin/Controllers/UsersController.php
+++ app/Admin/Controllers/UsersController.php
@@ -15,7 +15,7 @@ class UsersController extends AdminController
      *
      * @var string
      */
-    protected $title = 'User';
+    protected $title = '用户管理';
 
     /**
      * Make a grid builder.
@@ -26,14 +26,12 @@ class UsersController extends AdminController
     {
         $grid = new Grid(new User());
 
-        $grid->column('id', __('Id'));
-        $grid->column('name', __('Name'));
-        $grid->column('email', __('Email'));
-        $grid->column('email_verified_at', __('Email verified at'));
-        $grid->column('password', __('Password'));
-        $grid->column('remember_token', __('Remember token'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+        $grid->column('id', __('ID'));
+        $grid->column('name', __('姓名'));
+        $grid->column('phone', __('手机号'));
+        $grid->column('created_at', __('创建时间'));
+
+        $grid->disableCreateButton();
 
         return $grid;
     }
@@ -48,33 +46,12 @@ class UsersController extends AdminController
     {
         $show = new Show(User::findOrFail($id));
 
-        $show->field('id', __('Id'));
-        $show->field('name', __('Name'));
-        $show->field('email', __('Email'));
-        $show->field('email_verified_at', __('Email verified at'));
-        $show->field('password', __('Password'));
-        $show->field('remember_token', __('Remember token'));
-        $show->field('created_at', __('Created at'));
-        $show->field('updated_at', __('Updated at'));
+        $show->field('id', __('ID'));
+        $show->field('name', __('姓名'));
+        $show->field('phone', __('手机号'));
+        $show->field('created_at', __('创建时间'));
 
         return $show;
     }
 
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        $form = new Form(new User());
-
-        $form->text('name', __('Name'));
-        $form->email('email', __('Email'));
-        $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
-        $form->password('password', __('Password'));
-        $form->text('remember_token', __('Remember token'));
-
-        return $form;
-    }
 }
